f90f0382695e292230d8592e2a7990588d88d8ac
Roman <rrakhlin@gmail.com>
2020-04-29 16:53:30 +0300

    Initial Commit

---
+++ AceQR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AceQR
+//
+//  Created by Roman Rakhlin on 29.04.2020.
+//  Copyright © 2020 Roman Rakhlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
